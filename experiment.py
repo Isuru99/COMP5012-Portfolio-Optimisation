7bb1c2eea5070ec962c70bc8ed7c39354f23fe8e
Isuru Panditharatne <isuruip749@gmail.com>
2026-01-08 16:39:37 +0000

    Trying different data set

--- experiment.py
+++ experiment.py
@@ -3,9 +3,9 @@ from src.problem import PortfolioProblem
 from src.MOEA import optimise
 from src.plots import plot_pareto
 
-mu, cov = load_portfolio_data("data/port3.txt")
+mu, cov = load_portfolio_data("data/port1.txt")
 
 problem = PortfolioProblem(mu, cov)
-archive = optimise(problem, iterations=10000)
+archive = optimise(problem, iterations=5000)
 
 plot_pareto(archive)
