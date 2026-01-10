51b66ebb2bcfc5675865e50f52087db3f14eb41e
Isuru Panditharatne <isuruip749@gmail.com>
2026-01-10 16:30:43 +0000

    Add random seed control for reproducible experiments

--- experiment.py
+++ experiment.py
@@ -2,8 +2,11 @@ from src.data import load_portfolio_data
 from src.problem import PortfolioProblem
 from src.MOEA import optimise
 from src.plots import plot_pareto
+import numpy as np
 
-mu, cov = load_portfolio_data("data/port1.txt")
+mu, cov = load_portfolio_data("data/port3.txt")
+
+np.random.seed(42)
 
 problem = PortfolioProblem(mu, cov)
 archive = optimise(problem, iterations=5000)
