472adb8c1353dc9dc3be1b636938b9a895fa35db
Isuru Panditharatne <isuruip749@gmail.com>
2026-01-11 12:13:12 +0000

    Adjusting variables

--- src/problem.py
+++ src/problem.py
@@ -1,16 +1,17 @@
 import numpy as np
 
 class PortfolioProblem:
-    def __init__(self, mu, cov):
+    def __init__(self, mu, cov_matrix):
         self.mu = mu
-        self.cov = cov
+        self.cov_matrix = cov_matrix
         self.n_assets = len(mu)
 
     def evaluate(self, w):
         # Objective 1: maximise return → minimise negative return
-        f1 = -np.dot(w, self.mu)
+        returns = -np.dot(w, self.mu)
 
         # Objective 2: minimise risk (variance)
-        f2 = w.T @ self.cov @ w
+        #risk = w.T @ self.cov @ w
+        risk = np.dot(w.T, np.dot(self.cov_matrix, w))
 
-        return np.array([f1, f2])
+        return np.array([returns, risk])
